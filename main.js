fd5e710abe6c7aeb43a848dc649bafecb00d4268
HELLEN <hellen.castelan@escola.pr.gov.br>
2025-10-02 14:27:57 -0300

    .

--- main.js
+++ main.js
@@ -72,6 +72,7 @@ const stopMoveMario = (event) => {
         marioMovingLeft = false;
     }
 }
+startButton.addEventListener('click', startGame);
 
 const startGame = () => {
     gameStarted = true;
