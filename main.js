d1b02e4c6e31bf22747f4e43cd53ef4eb7b6935c
HELLEN <hellen.castelan@escola.pr.gov.br>
2025-10-06 07:45:19 -0300

    .

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const gameOverScreen = document.getElementById("game-over-screen");
 const scoreElement = document.querySelector(".score span");
 const monstro = document.querySelector(".monstro");
 const startButton = document.getElementById("startButton"); 
+const gameBoard = document.getElementById("game-board");
 const introScreen = document.querySelector(".intro-screen");
 
 let gameStarted = false;
